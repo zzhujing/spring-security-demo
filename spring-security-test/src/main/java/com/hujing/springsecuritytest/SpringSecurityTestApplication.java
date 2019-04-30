5fea64918eb64379dbca43fd17bee9b31f10f6c6
hujing <hj_960825@163.com>
2019-04-30 17:39:43 +0800

    update project build

--- spring-security-test/src/main/java/com/hujing/springsecuritytest/SpringSecurityTestApplication.java
+++ spring-security-test/src/main/java/com/hujing/springsecuritytest/SpringSecurityTestApplication.java
@@ -2,8 +2,9 @@ package com.hujing.springsecuritytest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SpringSecurityTestApplication {
 
     public static void main(String[] args) {
