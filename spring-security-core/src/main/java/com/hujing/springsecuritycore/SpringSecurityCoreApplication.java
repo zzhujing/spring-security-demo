0d22cf96d0b58c1f58ab3b83429d1af15854aaa8
hujing <hj_960825@163.com>
2019-04-30 17:06:25 +0800

    update

---
+++ spring-security-core/src/main/java/com/hujing/springsecuritycore/SpringSecurityCoreApplication.java
@@ -0,0 +1,13 @@
+package com.hujing.springsecuritycore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityCoreApplication.class, args);
+    }
+
+}
